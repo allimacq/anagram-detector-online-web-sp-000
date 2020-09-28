3d49e42991b75e1ddc5ee985e5700419a8db66e3
Allison MacQueen <allimacq@gmail.com>
2020-09-28 18:00:45 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,15 +7,17 @@ class Anagram
   end
   
   def match(words)
+    #initializing an array to hold anagrams
     value = [ ]
+    #sorting through the array of given words
     words.each do |word|
-      #p word.split("").sort
-      #p @word.split("").sort
+      #splitting up each word by letter and then sorting letters alphabetically and seeing if they match the initialized word 
       if word.split("").sort == @word.split("").sort
+        #if the letters match, we are adding them to the value array
         value << word
-        p value
       end
     end
+    #returning the value array from the loop since the each enumerable returns an unchanged array
     return value
   end
   
